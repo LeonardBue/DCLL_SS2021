916946fad169a1a3c454bbd12be246d426ac0f8f
Leonard Buechner <st149174@stud.uni-stuttgart.de>
2021-07-18 13:17:15 +0200

    Task 1.2 and 1.3

--- Library/Controllers/HZD_incomplete/targetEvolution.m
+++ Library/Controllers/HZD_incomplete/targetEvolution.m
@@ -3,7 +3,7 @@
 % 
 % INPUT:    th -- phase variable.
 % OUTPUT:   hD        -- vector of desired constraints for each actuated joint;
-%           dhD_th    -- derivative thereof wrt theta;
+%           dhD_dth    -- derivative thereof wrt theta;
 %           ddhD_ddth -- 2nd derivative thereof wrt theta.
 %
 function [hD,dhD_dth,ddhD_ddth] = targetEvolution(th)
@@ -41,42 +41,42 @@ function [hD,dhD_dth,ddhD_ddth] = targetEvolution(th)
 
     %%%% CODE 1.2.2 complete this (use constants defined above %%%%
     % The stance knee is kept to a fixed angle of beta_D, almost straight:
-    hD(1) = ChangeMe;
+    hD(1) = beta_offset + beta_D;
     % The stance hip is set such as to keep the MB pitch fixed:
-    hD(2) = ChangeMe;
+    hD(2) = phi_D - th;
     % The swing hip is set such as for the swing leg to mirror the stance leg:
-    hD(3) = ChangeMe;
+    hD(3) = - phi_D - th - hD(4)/2;
     %%%% End 1.2.2
 
     
     % Compute derivatives with respect to theta:
     %%%% CODE 1.3.1 complete this %%%%
     if th<=th_min
-        dhD_dth(4) = ChangeMe;
+        dhD_dth(4) = 0;
     elseif th<=0
-        dhD_dth(4) = ChangeMe;
+        dhD_dth(4) = 6*beta_max*th_min^(-3)*th^2 - 6*beta_max*th_min^(-2)*th;
     elseif th<=th_max
-        dhD_dth(4) = ChangeMe;
+        dhD_dth(4) = 6*beta_max*th_max^(-3)*th^2 - 6*beta_max*th_max^(-2)*th;
     else
-        dhD_dth(4) = ChangeMe;
+        dhD_dth(4) = 0;
     end
-    dhD_dth(1) = ChangeMe;
-    dhD_dth(2) = ChangeMe;
-    dhD_dth(3) = ChangeMe;
+    dhD_dth(1) = 0;
+    dhD_dth(2) = -1;
+    dhD_dth(3) = -1 - 1/2*dhD_dth(4);
 
     
     % Compute second derivatives:
     if th<=th_min
-        ddhD_ddth(4) = ChangeMe;
+        ddhD_ddth(4) = 0;
     elseif th<=0
-        ddhD_ddth(4) = ChangeMe;
+        ddhD_ddth(4) = 12*beta_max*th_min^(-3)*th - 6*beta_max*th_min^(-2);
     elseif th<=th_max
-        ddhD_ddth(4) = ChangeMe;
+        ddhD_ddth(4) = 12*beta_max*th_max^(-3)*th - 6*beta_max*th_max^(-2);
     else
-        ddhD_ddth(4) = ChangeMe;
+        ddhD_ddth(4) = 0;
     end
-    ddhD_ddth(1) = ChangeMe;
-    ddhD_ddth(2) = ChangeMe;
-    ddhD_ddth(3) = ChangeMe;
+    ddhD_ddth(1) = 0;
+    ddhD_ddth(2) = 0;
+    ddhD_ddth(3) = - 1/2*ddhD_ddth(4);
     %%%% End 1.3.1 %%%%
 end
